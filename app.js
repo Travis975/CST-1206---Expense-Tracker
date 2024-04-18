5890de3024bfe419dfb4602fd364e821b853bfae
Travis Schellenberg <travis.schellenberg@gmail.com>
2024-04-18 11:55:44 -0700

    finalized code and modified the PORT

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const UserRoutes = require('./routes/UserRoute');
 const ExpenseRoutes = require('./routes/expense');
 const path = require('path');
